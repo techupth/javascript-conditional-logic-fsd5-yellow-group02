5f25be6f29599aeac47962ae7be622da8d28f70b
panatyuki <admiral.dancho@gmail.com>
2023-11-30 13:22:52 +0700

    feat: add complete exercise

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,15 @@
 //Exercise 2
 let lightBulbStatus = "On";
+lightBulbStatus = "TechUP";
 
-// Start coding here.
+if (lightBulbStatus === "On") {
+  console.log("Light bulb is On.");
+} else if (lightBulbStatus === "Off") {
+  console.log("Light bulb is Off.");
+} else if (lightBulbStatus === "Broken") {
+  console.log("Light bulb is Broken.");
+} else {
+  console.log("Please choose the corrct input (On/Off/Broken)");
+}
+
+console.log(lightBulbStatus);
